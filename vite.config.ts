f910b526c1c4f7d16defbec53a9dbf04d81926ba
Aleksandr Shadrin <aleksandr.shadrin@sbermarket.ru>
2025-01-05 07:28:44 +0700

    feat: add gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/weather-for-yesterday/',
+  plugins: [react(), ghPages()],
 })
