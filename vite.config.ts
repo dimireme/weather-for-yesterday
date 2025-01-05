288f5cfefa5b0397984978caf43dd83e87119d1b
Aleksandr Shadrin <aleksandr.shadrin@sbermarket.ru>
2025-01-05 08:24:49 +0700

    feat: remove source maps from production

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import { ghPages } from 'vite-plugin-gh-pages';
 export default defineConfig({
   base: '/weather-for-yesterday/',
   plugins: [react(), ghPages()],
+  build: {
+    sourcemap: false,
+  },
 })
