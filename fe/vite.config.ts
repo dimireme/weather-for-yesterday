31f652dcf9af9286d27a61b814fd90b1ba9e54d1
dimireme <a_s_d@mail.ru>
2025-06-16 01:57:48 +0500

    fix: fix base path

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/weather-for-yesterday/',
   plugins: [react(), ghPages()],
   build: {
     sourcemap: false,
   },
-})
+});
