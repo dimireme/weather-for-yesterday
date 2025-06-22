e653016cf06982f21d7153785855ff1e64dd8c59
dimireme <a_s_d@mail.ru>
2025-06-23 01:02:02 +0500

    fix: fix basepath

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/weather-for-yesterday/' : '/',
   build: {
     sourcemap: false,
   },
-});
+}));
