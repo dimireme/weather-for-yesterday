0e27237eaaa13dd39db747dc9996e3a2b2ad5110
dimireme <a_s_d@mail.ru>
2025-06-29 13:58:19 +0500

    fix: fix api proxy for local dev

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: false,
   },
-}));
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+});
